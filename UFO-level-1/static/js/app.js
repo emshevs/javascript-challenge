6d2030701833748aaff2aae8e4a6583f8251177c
emshevs <64621476+emshevs@users.noreply.github.com>
2020-08-24 20:56:09 -0400

    added level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,37 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+
+tableData.forEach((incident) => {
+    console.log(incident);
+    var row = tbody.append("tr");
+
+    Object.entries(incident).forEach(([key,value]) => {
+        console.log(key,value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+button.on("click", function() {
+    d3.event.preventDefault();
+    tbody.html("");
+    var dateTime = d3.select("#datetime").property("value");
+    console.log(dateTime)
+    
+    var filteredData = tableData.filter(incident => incident.datetime === dateTime);
+    console.log(filteredData)
+
+    filteredData.forEach((incident) => {
+        var row = tbody.append("tr");
+
+        Object.entries(incident).forEach(([key, value]) => {
+            console.log(key, value);
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+
+});
